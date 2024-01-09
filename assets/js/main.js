abac2785567799dd73959b72f08e226b6da7b834
aldinpramudya <aldinarielpramudya@gmail.com>
2024-01-09 23:20:47 +0700

    feat : navbar and routing

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Show Nav Menu
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+// Menu Show
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu Hidden
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
