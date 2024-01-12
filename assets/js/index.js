838e36b755d4ad7bf1daaf020b025b6abfb7706d
aldinpramudya <aldinarielpramudya@gmail.com>
2024-01-12 12:00:52 +0700

    feat: add services

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// ==Services Modal==
+const modal = document.querySelectorAll('.services__modal')
+const modalButton = document.querySelectorAll('.services__button')
+const modalClose = document.querySelectorAll('.services__modal-close')
+
+let activeModal = (modalClick) => {
+    modal[modalClick].classList.add('active-modal')
+}
+
+modalButton.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', () => {
+        activeModal(i)
+    })
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modal.forEach((modalRemove) => {
+            modalRemove.classList.remove('active-modal')
+        })
+    })
+})
