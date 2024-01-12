f8d5960f1a9bf0aaeb0db385318188d41267b5f2
aldinpramudya <aldinarielpramudya@gmail.com>
2024-01-12 10:45:09 +0700

    feat: add about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,22 @@ const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 
 // Menu Show
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 // Menu Hidden
-if(navClose){
-    navClose.addEventListener('click', ()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
+// Change Background Header
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
