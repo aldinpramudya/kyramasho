de89133fa24cb32d117f198e2e345e3e9a571a92
aldinpramudya <aldinarielpramudya@gmail.com>
2024-03-08 12:33:27 +0700

    feat : add scroll action

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,10 @@ modalClose.forEach((modalClose) => {
         })
     })
 })
+
+// ==Show Scroll Up==
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
